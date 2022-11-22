947f3b3a506747c3332103a5bbf2431f9e443285
VAFAGIT <belgazzarwafae@gmail.com>
2022-11-22 10:06:02 +0100

    ff

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const dbConfig = require("./config/db");
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+// listen to port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
