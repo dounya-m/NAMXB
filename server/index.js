5562fa9928a316fbd00b86b003eecd373297089c
dounya-m <mellouki.dounya@gmail.com>
2022-11-22 17:04:32 +0100

    test

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const dbConfig = require("./config/db");
+const test = "test"
 
 app.use(cors());
 app.use(express.json());
