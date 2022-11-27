2f5eb72ffab1b9a51525945d7f6c4c47b20c4377
dounya-m <mellouki.dounya@gmail.com>
2022-11-27 14:32:10 +0100

    user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,48 @@
 const User = require('../models/UserModel')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { param } = require('../routes/usersRoutes');
 
-const getUsers = asyncHandler(async(req, res)=>{
+const getUsers = asyncHandler(async(req, res) => {
     const users = await User.find({})
     res.json(users)
 });
 
-module.exports = {getUsers}
+const addUser = asyncHandler(async(req,res) => {
+    const salt = await bcrypt.genSalt(10)
+
+    if(!req.body.name || !req.body.phone || !req.body.email || !req.body.password){
+            res.status(400).json({message:'this text is required'})
+    }
+    
+    const user = await User.create({
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email,
+        password: req.body.password
+    })
+    user.password = await bcrypt.hash(user.password, salt);
+    user.save();
+
+    res.status(200).json({user, token: generetToken(user._id)})
+});
+
+const login = asyncHandler(async(req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email})
+    if(!user){
+        res.status(400).json({message: 'this user dont exist'})
+    }
+    if(email && (await bcrypt.compare(password, user.password))){
+        res.json({user, token: generetToken(user._id)})
+    }
+    res.json('is logged')
+})
+
+const generetToken = (id) => {
+    const accssToken = jwt.sign({id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+    return({accssToken})
+}
+
+module.exports = {getUsers, addUser, login}
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,5 +3,7 @@ const user = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', user.getUsers)
+router.post('/', user.addUser)
+router.post('/login', user.login)
 
 module.exports = router
