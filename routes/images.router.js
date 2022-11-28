32215c59a3f8dead4de34860a2ac73cd893d6296
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2022-11-29 00:50:07 +0100

    edit router of images

--- routes/images.router.js
+++ routes/images.router.js
@@ -1,18 +1,8 @@
 const express = require("express");
 const controller = require("../controllers/imagesController");
-const multer = require("multer");
 const router = express.Router();
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "../userFiles/projectImage");
-  },
-  filename: function (req, file, cb) {
-    cb(null, "UploadedOn" + Date.now() + "fileOrigName" + file.originalname);
-  }
-});
-const upload = multer({ storage: storage });
 
-router.post("/", upload.single("image"), controller.saveProject);
+router.post("/",  controller.saveProject);
 //router.get('/edit', controller.create);
 //router.post('/create', controller.save);
 //router.get('/delete', controller.delete);
